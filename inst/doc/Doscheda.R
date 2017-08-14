69e28789b585a5e72355d4722ef5cc004e50a82b
brunocontrino <br1contrino@yahoo.co.uk>
2017-08-14 19:55:22 +0100

    update vignette

--- inst/doc/Doscheda.R
+++ inst/doc/Doscheda.R
@@ -16,7 +16,8 @@ dataTypeStr = 'intensity', modelTypeStr = 'linear',
     pearsonThrshVal = 0.4)
 
 ## ----setDataEx, eval = TRUE------------------------------------------------
-ex<- setData(x = ex, dataFrame = doschedaData,
+
+ex<- setData(x = ex, dataFrame = Doscheda::doschedaData,
   dataChannels = channelNames,
   accessionChannel = "Master.Protein.Accessions",
    sequenceChannel = 'Sequence',
@@ -31,6 +32,27 @@ ex <- removePeptides(ex,removePeps = FALSE)
 ## ----fitModelEx, eval = TRUE, include=FALSE--------------------------------
 ex<- runNormalisation(ex)
 ex <- fitModel(ex)
+
+## ----runDosEx, eval = FALSE------------------------------------------------
+#  
+#   channelNames <- c("Abundance..F1..126..Control..REP_1",
+#  "Abundance..F1..127..Sample..REP_1",  "Abundance..F1..128..Sample..REP_1",
+#  "Abundance..F1..129..Sample..REP_1",  "Abundance..F1..130..Sample..REP_1",
+#  "Abundance..F1..131..Sample..REP_1",  "Abundance..F2..126..Control..REP_2",
+#  "Abundance..F2..127..Sample..REP_2", "Abundance..F2..128..Sample..REP_2",
+#  "Abundance..F2..129..Sample..REP_2",  "Abundance..F2..130..Sample..REP_2",
+#   "Abundance..F2..131..Sample..REP_2")
+#  
+#  ex <- runDoscheda(dataFrame = doschedaData, dataChannels = channelNames,
+#   chansVal = 6, repsVal = 2,dataTypeStr = 'intensity',
+#   modelTypeStr = 'linear',PDBool = FALSE,removePepsBool = FALSE,
+#   accessionChannel = "Master.Protein.Accessions",
+#   sequenceChannel = 'Sequence',qualityChannel = "Qvality.PEP",
+#   incPDofPDBool = FALSE, incGeneFileBool = FALSE,
+#    organismStr = 'H.sapiens', pearsonThrshVal = 0.4)
+#  
+#  runDoscheda()
+#  
 
 ## ----plot1-----------------------------------------------------------------
 plot(ex)
